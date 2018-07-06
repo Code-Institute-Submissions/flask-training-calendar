794227b2f4796afad3a3174c52b1f67f5f1282b7
Michael Park <mparkcode@gmail.com>
2018-07-06 13:31:00 +0000

    Added home and workouts route

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 import os
 
 app = Flask(__name__)
@@ -9,30 +8,32 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
-    
-    def __repr__(self):
-        return '<User %r>' % self.username 
-        
-class Workout(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(100), nullable=False)
-    target_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    description = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    
-    def __repr__(self):
-        return '<Post %r>' % self.title
+workouts = [
+    {
+        'type': '5k Run',
+        'description': '5k Run',
+        'target_date': 'July 06, 2018'
+    },
+    {
+        'type': '3k Workout Run',
+        'description': '3k Tempo Run',
+        'target_date': 'July 08, 2018'
+    },
+    {
+        'type': '10K Long Run',
+        'description': '10K Long slow run',
+        'target_date': 'July 10, 2018'
+    }
+]
+
 
 @app.route('/')
 def hello():
-    return "Hello"
+    return render_template("home.html")
+    
+@app.route('/workouts')
+def get_workouts():
+    return render_template("workouts.html", workouts=workouts, title="workouts")
     
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
---
+++ models.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+    
+    def __repr__(self):
+        return '<User %r>' % self.username 
+        
+class Workout(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(100), nullable=False)
+    target_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    description = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    def __repr__(self):
+        return '<Post %r>' % self.title
