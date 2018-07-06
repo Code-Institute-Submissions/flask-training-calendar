ea2d895de73b01195caec058c21370d86c090f9d
Michael Park <mparkcode@gmail.com>
2018-07-06 14:29:29 +0000

    Added main.css and initial styling

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_s3 import FlaskS3
 import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY']=os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['FLASKS3_BUCKET_NAME'] = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+s3 = FlaskS3(app)
 
 workouts = [
     {
