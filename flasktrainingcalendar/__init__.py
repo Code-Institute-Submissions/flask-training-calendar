6dc76e56fa03b90262f0ac4d53732b8a113859fe
Michael Park <mparkcode@gmail.com>
2018-07-10 12:37:05 +0000

    Added login and logout functionality

--- flasktrainingcalendar/__init__.py
+++ flasktrainingcalendar/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_s3 import FlaskS3
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 import os
 
 app = Flask(__name__)
@@ -9,5 +11,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 app.config['FLASKS3_BUCKET_NAME'] = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 s3 = FlaskS3(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
 
 from flasktrainingcalendar import routes
--- flasktrainingcalendar/routes.py
+++ flasktrainingcalendar/routes.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, url_for, flash, redirect
-from flasktrainingcalendar import app
+from flasktrainingcalendar import app, db, bcrypt
 from flasktrainingcalendar.models import User, Workout
 from flasktrainingcalendar.forms import RegistrationForm, LoginForm
+from flask_login import login_user, current_user, logout_user
 
 workouts = [
     {
@@ -33,19 +34,42 @@ def get_workouts():
     
 @app.route('/register', methods=['POST', "GET"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
+        hash_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username = form.username.data, email = form.email.data, password = hash_password)
+        db.session.add(user)
+        db.session.commit()
         flash("Your account has been successfully created, please login", "success")
         return redirect(url_for("home"))
     return render_template("register.html", title="register", form=form)
     
 @app.route('/login', methods=["POST", "GET"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if (form.username_or_email.data == 'admin@example.com' or form.username_or_email.data == 'admin') and form.password.data == "password":
+        user = User.query.filter_by(email = form.username_or_email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
+        if user is None:
+            user = User.query.filter_by(username = form.username_or_email.data).first()
+            if user and bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                flash('You have been logged in!', 'success')
+                return redirect(url_for('home'))
+            else:
+                flash('Login Unsuccessful. Please check your login details', 'danger')
         else:
             flash('Login Unsuccessful. Please check your login details', 'danger')
     return render_template("login.html", title="login", form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
