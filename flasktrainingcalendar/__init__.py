09e653415c62db129612fb2975e2136f89be5cd8
Michael Park <mparkcode@gmail.com>
2018-07-10 14:25:52 +0000

    Installed psycopg2

--- flasktrainingcalendar/__init__.py
+++ flasktrainingcalendar/__init__.py
@@ -8,7 +8,6 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY']=os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 db = SQLAlchemy(app)
 app.config['FLASKS3_BUCKET_NAME'] = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 s3 = FlaskS3(app)
