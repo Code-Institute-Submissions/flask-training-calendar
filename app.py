41e52f34082d6ee0f4146cbbdd19fca13fe0c368
Michael Park <mparkcode@gmail.com>
2018-07-10 08:27:03 +0000

    Added login & Register routes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
 from flask_s3 import FlaskS3
 import os
@@ -36,6 +37,16 @@ def hello():
 @app.route('/workouts')
 def get_workouts():
     return render_template("workouts.html", workouts=workouts, title="workouts")
+    
+@app.route('/register')
+def register():
+    form = RegistrationForm
+    return render_template("register.html", title="register", form=form)
+    
+@app.route('/login')
+def login():
+    form = LoginForm
+    return render_template("login.html", title="login", form=form)
     
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
